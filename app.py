0a7a3e5e5b065d9184b7caf487e94d3d552bc1b5
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2025-02-01 01:34:04 +0530

    Fixed and added pages to flask app

--- app.py
+++ app.py
@@ -4,13 +4,25 @@
 #     "flask",
 # ]
 # ///
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__,template_folder="templates",static_folder="static")
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/plan')
+def plan():
+    return render_template('plan.html')
+
+@app.route('/myprojects')
+def myprojects():
+    return render_template('myprojects.html')
+
+@app.route('/faqs')
+def faqs():
+    return render_template('FAQs.html')
 
 if(__name__=="__main__"):
     app.run(host="127.0.0.1",port=8080,debug=True)
